d0898f8ee4bef4e4232d18334f1205ce8a0202e8
cngzylmz89 <muallimiturki@gmail.com>
2025-05-28 10:51:28 +0300

    dataacceslayerdalpersonel

---
+++ EntityLayer/EntityPersonel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+   public class EntityPersonel
+    {
+        int ID;
+        string Ad;
+        string Soyad;
+        string Sehir;
+        string gorev;
+        short Maas;
+
+        public int ID1 { get => ID; set => ID = value; }
+        public string Ad1 { get => Ad; set => Ad = value; }
+        public string Soyad1 { get => Soyad; set => Soyad = value; }
+        public string Sehir1 { get => Sehir; set => Sehir = value; }
+        public string Gorev { get => gorev; set => gorev = value; }
+        public short Maas1 { get => Maas; set => Maas = value; }
+    }
+}
