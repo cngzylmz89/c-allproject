a47854a1e60ef2206feb5db5973bbfce0026f1a0
cngzylmz89 <muallimiturki@gmail.com>
2025-06-05 12:03:19 +0300

    DBOGRENCINOT and sqlquerry1

---
+++ SQLVERITABANLARI/SQLQuery1.sql
@@ -0,0 +1,10 @@
+--create Table TBLDERSLER-->//TBLDERSLER adında tablo oluştur
+--(
+
+-- DERSID int,-->column adı DERSID veri türü int olarak oluştur
+-- DERSAD varchar(20)-->varchar kullan ve en fazla 20 karakter olsun.
+
+--)
+
+--ALTER TABLE TBLDERSLER  -->TBLDERSLER i güncelle
+--Drop column Kontenjan  -->Kontenjan adındaki columu kaldır
